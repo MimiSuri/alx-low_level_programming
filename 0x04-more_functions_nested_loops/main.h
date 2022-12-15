38430c6843e4dee8ec29c5a44749e267ec78dc35
MimiSuri <miimiisurii@yahoo.com>
2022-12-15 13:00:45 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+int _pow(int a, int b);
 int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
@@ -12,6 +13,7 @@ void print_line(int n);
 void print_square(int size);
 void print_diagonal(int n);
 void print_triangle(int size);
+void print_number(int n);
 void print_number(int n);
 
 #endif
