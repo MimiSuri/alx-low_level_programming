5b7864c0fb2981df35ff9974e44176a0ce6290f1
MimiSuri <miimiisurii@yahoo.com>
2022-12-20 00:59:35 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,18 +5,12 @@
 /**
  *
  * reset_to_98 -main entry.
- *
  * void: nothing
- *
  * @n: integer pointer
- *
  * Description:  function that takes a pointer to an int as parameter
- *
  * and updates the value it points to to 98
- *
  * Return: void
- *
- **/
+ */
 
 void reset_to_98(int *n)
 
