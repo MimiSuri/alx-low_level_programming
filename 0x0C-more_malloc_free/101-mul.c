a6f8ef1a543b6daa770fd76a7675a452e3947c14
MimiSuri <miimiisurii@yahoo.com>
2023-01-12 12:11:15 -0800

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,6 +9,11 @@
 * Return: 0 success
 */
 int main(int argc, char *argv[])
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 {
 unsigned long mul;
 int i, j;
