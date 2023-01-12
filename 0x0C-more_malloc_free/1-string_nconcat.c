e5645b3215c0be41e5578a2ebf15d667a092fd5d
MimiSuri <miimiisurii@yahoo.com>
2023-01-12 11:34:48 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,25 +3,43 @@
 #include "main.h"
 
 /**
- * _calloc - print 0s into 2D grid
- * @nmemb: array num
- * @size: size of ea array element
- * Return: 0
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i;
+	char *concat;
+	unsigned int len = n, index;
 
-	if (nmemb == 0 || size == 0)
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
 
-	arr = malloc(nmemb * size);
-	if (arr == NULL)
-		return (NULL);
+	len = 0;
 
-	for (i = 0; i < (nmemb * size); i++)
-		arr[i] = 0;
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
 
-	return (arr);
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
